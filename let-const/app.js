2cc0a12dd79d03a07f4a9a472b4240bf1e7d1626
SpencerPulliam <spencer_pulliam@yahoo.com>
2021-07-26 19:18:05 -0500

    let and const practice

---
+++ let-const/app.js
@@ -0,0 +1,30 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14 // this would disallow the reassignment or redeclaration of the variable PI
+
+// What is the difference between var and let?
+
+/*
+var allows for reassignment and redeclaration, whereas let only allows reassignment, but not redeclatration.
+*/
+
+// What is the difference between var and const?
+
+/*
+var allows for reassignment and redeclaration, but const allows neither of these things.
+*/
+
+// What is the difference between let and const?
+
+/*
+let allows reassignment, but const does not.
+*/
+
+// What is hoisting?
+
+/*
+Javascript moves variable and function declarations to the top of the block they are contained in
+so that they can be invoked regardless of their position in the block without causing an error
+*/
