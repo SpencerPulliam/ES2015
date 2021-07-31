7d3f31e741306e108c832fb24f436054bcab465b
SpencerPulliam <spencer_pulliam@yahoo.com>
2021-07-31 18:40:32 -0500

    forgot to save last changes before commit

--- objEnhancements/app.js
+++ objEnhancements/app.js
@@ -44,3 +44,19 @@ const instructor = {
     sayHi() {return 'Hi!'},
     sayBye() {return this.firstName + ' says bye!';}
 }
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+function generateAnimal(species, verb, noise) {
+    return {
+        speices,
+        [verb]() {
+            return noise;
+        }
+    }
+}
